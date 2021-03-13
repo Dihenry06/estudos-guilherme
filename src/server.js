a31c52a365835c1f2773e2629aceab006e01e60b
Diogo Henrique <diogo@Diogos-MacBook-Pro.local>
2021-03-13 01:12:47 -0300

    add login and validationn if user exists

--- src/server.js
+++ src/server.js
@@ -2,8 +2,6 @@ const express = require('express');
 const mongoose = require('mongoose');
 const morgan = require('morgan');
 
-morgan('dev');
-
 mongoose
   .connect('mongodb://localhost:27017/teste2', {
     useNewUrlParser: true,
@@ -35,11 +33,21 @@ const UserSchema = new mongoose.Schema({
 mongoose.model('User', UserSchema);
 
 const app = express();
+
 app.use(express.json());
+app.use(morgan('dev'));
 
 app.post('/user', async (req, res) => {
   const UserSchema = mongoose.model('User');
   const { email, password } = req.body;
+
+  //busca para ver se email já esta cadastrado
+  const ifexists = await UserSchema.findOne({ email });
+
+  //valido se a variavel @ifexists é true, se sim ele cai na validação
+  if (ifexists) {
+    return res.status(404).json({ mesage: 'email ja cadastrado' });
+  }
 
   const response = await UserSchema.create({
     email,
@@ -47,6 +55,19 @@ app.post('/user', async (req, res) => {
   });
 
   return res.status(201).json({ message: 'Usuario criado com sucesso' });
+});
+
+app.post('/login', async (req, res) => {
+  const UserSchema = mongoose.model('User');
+  const { email, password } = req.body;
+
+  const response = await UserSchema.findOne({ email, password });
+
+  if (!response) {
+    return res.status(404).json({ mesage: 'usuario nao encontrado' });
+  }
+
+  return res.status(200).send();
 });
 
 app.listen(8080, () => {
